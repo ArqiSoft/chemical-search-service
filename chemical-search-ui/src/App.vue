8801acd5a737e85b6c8fa42bfafede396298afc4
Rick Zakharov <rick.zakharov@gmail.com>
2020-12-18 15:50:40 -0500

    Simple front-end in progress

---
+++ chemical-search-ui/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Home msg="Checmical Search"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Home from './components/Home.vue';
+
+@Options({
+  components: {
+    Home,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
