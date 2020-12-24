00180e60fa59191ebbf832fba59f87ba93d31031
Denis V Pogulyaev <denispogulyaev@gmail.com>
2020-12-24 21:13:36 +0300

    Add initial ui

---
+++ ui/README.md
@@ -0,0 +1,29 @@
+# chemical-search-ui
+
+## Project setup
+
+```terminal
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```terminal
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```terminal
+npm run build
+```
+
+### Lints and fixes files
+
+```terminal
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ ui/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <a href="http://www.arqisoft.com/" alt="ArqiSoft, LLC" target="_blank" rel="noopener noreferrer">
+    <img alt="ArqiSoft logo" src="./assets/logo.png" class="logo">
+  </a>
+  <Home msg="Checmical Search"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Home from './components/Home.vue';
+
+@Options({
+  components: {
+    Home,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  width: 10rem;
+}
+</style>
