4716513d8a612ba23cc5b44ce365ede95d4a09d5
Ed Dagaev <ed.dagaev@gmail.com>
2021-01-14 18:47:35 +0300

    add menu

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,93 @@
 <template>
   <div id="app">
-    <a href="http://www.arqisoft.com/" alt="ArqiSoft, LLC" target="_blank" rel="noopener noreferrer">
-      <img alt="ArqiSoft logo" src="./assets/images/logo.png" class="logo">
+    <header class="navbar">
+      <section class="navbar-center"></section>
+      <section class="navbar-section m-2">
+        <a alt="About" href="#" class="btn btn-link" @click="isActive = true"
+          >About</a
+        >
+        <a
+          alt="GitHub Repo"
+          class="btn btn-link"
+          href="https://github.com/ArqiSoft/chemical-search-service"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="ArqiSoft logo"
+            src="./assets/images/github-brands.svg"
+            width="24"
+            title="GitHub Repo"
+          />
+        </a>
+      </section>
+    </header>
+    <a
+      alt="ArqiSoft, LLC"
+      href="http://www.arqisoft.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img alt="ArqiSoft logo" src="./assets/images/logo.png" class="logo" />
     </a>
-    <Search header="Chemical Search"/>
+    <Search header="Chemical Search" />
+
+    <div class="modal" id="modal-id" :class="{ active: isActive }">
+      <a href="#close" class="modal-overlay" aria-label="Close"></a>
+      <div class="modal-container">
+        <div class="modal-header">
+          <a
+            href="#close"
+            class="btn btn-clear float-right"
+            aria-label="Close"
+            @click="isActive = false"
+          ></a>
+          <div class="modal-title h5">About</div>
+        </div>
+        <div class="modal-body">
+          <div class="content">
+            <p>
+              Chemical Search is a demo application to evaluate chemical
+              structure searches performance running
+              <a
+                alt="Bingo API with ElasticSearch"
+                href="https://github.com/epam/Indigo/tree/master/api/plugins/bingo-elastic/java"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Bingo API with ElasticSearch</a
+              >. This application has been built for research purposes only and
+              can be stopped any time.
+            </p>
+            <p>
+              Publicly available
+              <a
+                alt="SureChEMBL dataset"
+                href="ftp://ftp.ebi.ac.uk/pub/databases/chembl/SureChEMBL/data/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >SureChEMBL dataset</a
+              >
+              is preloaded into ElasticSearch and current index contains more
+              than 20 millions compounds.
+            </p>
+            <p>
+              The application hosted in AWS environment on a separate EC2
+              instance with ElasticSearch 7 running in Docker but some other
+              configurations may be applied later.
+            </p>
+            <p>
+              <a
+                alt="GitHub Repo"
+                href="https://github.com/ArqiSoft/chemical-search-service"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Source code</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,11 +100,13 @@ import Search from './components/Search.vue';
     Search,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  isActive = false;
+}
 </script>
 
 <style lang="scss">
-@import "./assets/styles/styles";
+@import './assets/styles/styles';
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -32,5 +118,9 @@ export default class App extends Vue {}
 .logo {
   width: 10rem;
   text-align: center;
+}
+
+#modal-id {
+  text-align: left;
 }
 </style>
